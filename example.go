4cd5666438acbc6e37d51db1719a2898926dde49
flaster <harifrahman999@gmail.com>
2021-12-30 14:46:22 +0700

    feat(init): gin handler

--- example.go
+++ example.go
@@ -7,18 +7,22 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello there!",
-			"creator": "Arif Rahman Hakim",
-		})
-	})
+	r.GET("/", rootHandler)
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", pingHandler)
 
 	r.Run(":9090") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello there!",
+		"creator": "Arif Rahman Hakim",
+	})
+}
+
+func pingHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
 }
